b7e46d32b4592df59c3414b22402a2f6a7b07a88
Jan Erik <janerik.kodehode@gmail.com>
2024-11-06 10:56:01 +0100

    Fixed animation issues

--- script.js
+++ script.js
@@ -18,12 +18,7 @@ const drumkit = [
   "l",
 ];
 
-// const flash = () => {
-//   if (drumId === "tom") {
-//     drumDiv.id.style.backgroundColor = "red";
-//   }
-// };
-
+// Loop to create divs
 for (let i = 0; i < drumkit.length; i += 2) {
   const drumDiv = document.createElement("div");
   const drumTitle = document.createElement("p");
@@ -31,35 +26,56 @@ for (let i = 0; i < drumkit.length; i += 2) {
   drumTitle.textContent = `${drumkit[i]} (${drumkit[i + 1]})`;
   drumDiv.id = drumkit[i].toLowerCase();
   drumDiv.classList.add("drums");
+  drumCombined.classList.add("drum-combined");
 
+  drumDiv.style.marginBottom = `${i * 2}px`;
+
+  // Click event listener
   drumDiv.addEventListener("click", () => {
     new Audio(`./sounds/${drumkit[i].toLowerCase()}.wav`).play();
+
+    // Animation. Removing and adding class to reset animation. Delay needed for it to work.
+    drumDiv.classList.remove("drum-hit");
+
+    setTimeout(() => {
+      drumDiv.classList.add("drum-hit");
+    }, 1);
   });
 
   drumCombined.append(drumTitle, drumDiv);
   drumArea.append(drumCombined);
 }
 
+// Keypress event listener
 window.addEventListener("keydown", (e) => {
   const drum = drumkit.indexOf(e.key) - 1;
   if (drumkit.includes(e.key)) {
-    new Audio(`./sounds/${drumkit[drum].toLowerCase()}.wav`).play();
     const drumId = document.querySelector(`#${drumkit[drum].toLowerCase()}`);
-    // drumId.style.backgroundColor = "red";
 
-    let activate = 100;
+    new Audio(`./sounds/${drumkit[drum].toLowerCase()}.wav`).play();
 
-    const flash = () => {
-      console.log(activate);
-      if (activate === 0) {
-        clearInterval(interval);
-      } else {
-        activate--;
-        drumId.style.backgroundColor = `rgba(255, 255, 255, ${activate}%)`;
-        drumId.style.width = `${75 + activate / 10}px`;
-        drumId.style.height = `${75 + activate / 10}px`;
-      }
-    };
-    const interval = setInterval(flash, 0.5);
+    // Animation. Removing and adding class to reset animation. Delay needed for it to work.
+    drumId.classList.remove("drum-hit");
+
+    setTimeout(() => {
+      drumId.classList.add("drum-hit");
+    }, 1);
+
+    // drumId.addEventListener("");
+
+    // let activate = 100;
+
+    // const flash = () => {
+    //   console.log(activate);
+    //   if (activate === 0) {
+    //     clearInterval(interval);
+    //   } else {
+    //     activate--;
+    //     drumId.style.backgroundColor = `rgba(255, 255, 255, ${activate}%)`;
+    //     drumId.style.width = `${75 + activate / 10}px`;
+    //     drumId.style.height = `${75 + activate / 10}px`;
+    //   }
+    // };
+    // const interval = setInterval(flash, 0.5);
   }
 });
